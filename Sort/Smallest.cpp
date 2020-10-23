fb8f81035ce0312369017b3e2d2aeb1d361448c6
aleesain <70762730+aleesain@users.noreply.github.com>
2020-10-22 23:40:44 -0800

    Added Smallest and Greatest
    The user will provide 5 values in the array and the program will print the smallest and greatest value in that array respectively.

---
+++ Sort/Smallest.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	// This program will print out the smallest number in an array of 5 numbers provided by the user.
+	const int s=5; 
+	int A[s], d=0;
+	cout<<"Enter values of array: ";
+	for(int i=0;i<s;i++){
+		cin>>A[i];
+	}
+	cout<<"The smallest number is: ";
+	for(int e=0;e<=1;e++){
+	for(int c=0; c<=4; c++){
+		if(A[d]>A[c]){
+			d++;
+		}
+	}
+}
+cout<<A[d];
+}
